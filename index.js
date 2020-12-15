3f7ef86bd2aa7cf76de8161d70859a6a8b0f6bbf
PRAKASH <PRAKASH@SANKHI.com>
2020-12-16 00:38:29 +0100

    footer updated

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
+app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('aboutMe.ejs');
