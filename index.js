7864382901b2d4416ebb8271df856ceb3aa9458a
PRAKASH <PRAKASH@SANKHI.com>
2020-12-17 20:28:34 +0100

    add only heroku server port and try run

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-
+const port = process.env.port;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
@@ -26,7 +26,8 @@ app.get('/education', (req, res) => {
     res.render('education.ejs');
 })
 
+app.listen(port);
 
-app.listen(process.env.port || 3000, () => {
-    console.log("Listening on port: 3000");
-}) 
+// app.listen(process.env.port || 3000, () => {
+//     console.log("Listening on port: 3000");
+// }) 
