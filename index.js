ccdc3b759e405f1ba5ba587d4e967777a6f8da4b
PRAKASH <PRAKASH@SANKHI.com>
2020-12-17 18:33:31 +0100

    ready to deploye

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 app.use('/public', express.static('public'));
 
+
 app.get('/', (req, res) => {
     res.render('aboutMe.ejs');
 })
