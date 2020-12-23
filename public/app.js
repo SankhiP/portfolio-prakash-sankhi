28f8792c9dcc65bd46e640b755c1a7c064575d16
PRAKASH <PRAKASH@SANKHI.com>
2020-12-23 18:24:53 +0100

    Responsive Complete

--- public/app.js
+++ public/app.js
@@ -1,4 +1,29 @@
-// const text = document.querySelectorAll('h1');
-// text.style.color = "red";
+
+
+async function makeActive() {
+    const currents = document.querySelectorAll('header .btn');
+
+    for (const current of currents) {
+        const backgroundColor = "blue";
+        current.style.backgroundColor;
+        this.className += backgroundColor;
+    }
+}
+// current.addEventListener('click', function () {
+
+
+// current.className = current.className.replace(" active", "");
+// this.className += " active";
+
+
+
+
+const btns = document.querySelectorAll('header .btn');
+
+for (const btn of btns) {
+    btn.addEventListener("click", async function () {
+        await makeActive();
+    });
+}
 
 
